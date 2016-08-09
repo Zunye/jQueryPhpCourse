37d68ae03df4a23450e8f30a885856b6b9ac6ea0
Benjamin Bela Stuerz <b.stuerz@gmx.de>
2016-08-09 05:20:25 +0200

    emptry framework in index.php

--- index.php
+++ index.php
@@ -1,7 +1,45 @@
 <!DOCTYPE html>
 	<head>
-		<title>Moomie</title>
+		<title>Moomins - Fun in Helsinki</title>
+		
+		<!--Meta Tags-->
+		<meta charset="UTF-8">
+		<meta name="description" content="Moomins - Fun in Helsinki with nice stories">
+		<meta name="keywords" content="Moomins,Helsinki,Traveltips,Stories">
+		<meta name="author" content="Zarina, Menno, Benjamin">
+		<!--End of Meta Tags-->
+		
+		<!--External CSS and JavaScript Files-->
+		<link rel="stylesheet" href="css/styles.css">
+		<script src="script.js"></script>
+		<!--End of external files-->
+		<!--jQuery from Google-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<!--End of jQuery from Google-->
+		
 	</head>
 	<body>
+		<header>
+			<a href="login.php" class="button">Login-Button</a>
+		</header>
+		<div class="mainPage">
+			<p>Some Text with an Image</p>
+		</div>
+		<div class="storyShortcut">
+			<a href="#story1">Story 1</a>
+			<a href="#story2">Story 2</a>
+			<a href="#story3">Story 3</a>
+		</div>
+		<div class="storyPages">
+			<div id="story1">
+				<p>First story</p>
+			</div>
+			<div id="story2">
+				<p>Second story</p>
+			</div>
+			<div id="story3">
+				<p>Third story</p>
+			</div>
+		</div>
 	</body>
 </html>
